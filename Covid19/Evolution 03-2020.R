233442a03d6b0987e3d7699efed948c691a4e549
orlindowagner <orlindowagner@gmail.com>
2020-04-04 10:12:28 -0300

    atualização do database e comentarios no script

--- Covid19/Evolution 03-2020.R
+++ Covid19/Evolution 03-2020.R
@@ -6,6 +6,8 @@ dados
 colnames(dados) <- c("dia_mes", "dia","cont_dia", "mort_dia")
 dados
 
+#TENTATIVA DE APLICAR A REGRESSÃO LINEAR NOS DADOS DE TAXAS DIÁRIA
+
 lm(cont_dia ~dia, data = dados)
 summary(lm(cont_dia ~dia, data = dados))
 
@@ -19,7 +21,7 @@ plot(dados$dia,dados$mort_dia)
 
 ## FAZENDO OS ACUMULADOS EM CONTAMINADO E MORTES
 
-
+# Acumulado no numero de contaminados
 acum_cont <- c()
 
 acum_cont[1] <- dados$cont_dia[1]
@@ -33,7 +35,7 @@ for (i in 2:nrow(dados)){
 acum_cont
 
 
-
+# Acumulado no numero de mortes
 acum_mort <- c()
 
 acum_mort[1] <- dados$mort_dia[1]
@@ -56,21 +58,20 @@ summary(lm(acum_cont ~dia, data = dados))
 lm(acum_mort ~dia, data = dados)
 summary(lm(acum_mort ~dia, data = dados))
 
-## PLOTANDO GRAFICOS
+
+## PLOTANDO GRAFICOS DOS VALORES ACUMULADOS
 
 plot(dados$dia,acum_cont)
 plot(dados$dia,acum_mort)
 
 
-
+## AVALIANDO TAXA DE LETALIDADE
 taxa_diaria <- c()
 taxa_diaria <- acum_mort / acum_cont * 100
 taxa_diaria
 plot(dados$dia,taxa_diaria)
 
 
-
-
-
-
-
+## CONFERINDO DADOS ATUALIZADOS
+acum_cont
+acum_mort
